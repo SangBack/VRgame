e8111d6713e12e5996b37493e3dd85e64a3be059
SangBack <71425511+SangBack@users.noreply.github.com>
2021-02-08 20:16:58 +0900

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public ParticleSystem BulletHit;
+    public Transform BulletHitPivot;
+    public int damage;
+
+    void Update()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            ParticleSystem BulletHitPre = Instantiate(BulletHit, BulletHitPivot.position, BulletHitPivot.rotation);
+            BulletHitPre.Play();
+            Destroy(gameObject);
+        }
+
+    }
+
+
+}
